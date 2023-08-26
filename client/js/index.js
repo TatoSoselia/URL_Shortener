0e43c40a599ea0007162c60318a95220df01c814
TatoSoselia <tatososelia93@gmail.com>
2023-08-26 13:29:36 +0400

    Improved return shorten_url

--- client/js/index.js
+++ client/js/index.js
@@ -5,6 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const copyButton = document.getElementById('copyButton');
     const urlInput = document.querySelector('input[name="url"]'); // Get the input element
 
+    // Function to shorten a URL relative to the hosting URL
+    function shorten_url(url) {
+        // Get the hosting URL (base URL)
+        var baseURL = window.location.origin;
+
+        // Check if the input URL starts with the baseURL
+        if (url.startsWith(baseURL)) {
+            // If it does, return the relative path
+            return url.substring(baseURL.length);
+        } else {
+            // If not, return the input URL as is
+            return url;
+        }
+    }
+
     shortenForm.addEventListener('submit', function(event) {
         event.preventDefault();
 
@@ -24,13 +39,15 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             if (data.short_url) {
-                shortUrlLink.href = data.short_url;
-                shortUrlLink.textContent =  data.short_url;
+                const shortenedURL = shorten_url(data.short_url); // Shorten the URL
+
+                shortUrlLink.href = shortenedURL; // Set the shortened URL as the link href
+                shortUrlLink.textContent = shortenedURL; // Display the shortened URL
                 shortenedUrlDiv.style.display = 'block';
 
                 copyButton.addEventListener('click', function() {
                     const textArea = document.createElement('textarea');
-                    textArea.value = data.short_url;
+                    textArea.value = shortenedURL;
                     document.body.appendChild(textArea);
                     textArea.select();
                     document.execCommand('copy');
@@ -38,13 +55,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
                     copyButton.textContent = 'Copied!';
 
-
                     // Reset after a few seconds (e.g., 3 seconds)
                     setTimeout(function() {
                         copyButton.textContent = 'Copy';
-                        // shortenedUrlDiv.style.display = 'none';
                         urlInput.value = ''; // Clear the input field again
-                    }, 2000); // 3000 milliseconds = 3 seconds
+                    }, 2000); // 2000 milliseconds = 2 seconds
                 });
             }
         })
